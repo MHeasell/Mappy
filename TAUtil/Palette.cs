4832d8c3629b08a631d36ede1dead4aaafa1a552
ArmouredFish <armouredfish@gmail.com>
2013-12-23 02:50:20 +0000

    Extract palette reading code into PaletteReader class

--- TAUtil/Palette.cs
+++ TAUtil/Palette.cs
@@ -1,5 +1,6 @@
 ﻿namespace TAUtil
 {
+    using System;
     using System.Drawing;
     using System.IO;
 
@@ -7,30 +8,18 @@
     {
         public static Color[] LoadArr(Stream file)
         {
-            StreamReader s = new StreamReader(file);
-            string magic1 = s.ReadLine();
-            string magic2 = s.ReadLine();
-            if (!magic1.Equals("JASC-PAL")
-                || !magic2.Equals("0100"))
-            {
-                throw new IOException("Unrecognised palette format");
-            }
+            PaletteReader r = new PaletteReader(new StreamReader(file));
 
-            string entries = s.ReadLine();
-            if (!entries.Equals("256"))
+            if (r.ColorsCount != 256)
             {
-                throw new IOException("This palette is not 256 colors");
+                throw new ArgumentException("Palette is not 256 colors");
             }
 
             Color[] pal = new Color[256];
+
             for (int i = 0; i < 256; i++)
             {
-                string line = s.ReadLine();
-                string[] fields = line.Split(' ');
-                pal[i] = Color.FromArgb(
-                    int.Parse(fields[0]),
-                    int.Parse(fields[1]),
-                    int.Parse(fields[2]));
+                pal[i] = r.ReadColor();
             }
 
             return pal;
---
+++ TAUtil/PaletteReader.cs
@@ -0,0 +1,68 @@
+﻿namespace TAUtil
+{
+    using System;
+    using System.Drawing;
+    using System.IO;
+
+    public class PaletteReader : IDisposable
+    {
+        private readonly TextReader reader;
+
+        public PaletteReader(TextReader reader)
+        {
+            this.reader = reader;
+
+            string magic1 = reader.ReadLine();
+            string magic2 = reader.ReadLine();
+
+            if (magic1 != "JASC-PAL" || magic2 != "0100")
+            {
+                throw new ParseException("Unrecognised palette format");
+            }
+
+            string entries = reader.ReadLine();
+
+            if (entries == null)
+            {
+                throw new ParseException("File ended prematurely");
+            }
+
+            this.ColorsCount = int.Parse(entries);
+        }
+
+        ~PaletteReader()
+        {
+            this.Dispose(false);
+        }
+
+        public int ColorsCount { get; private set; }
+
+        public Color ReadColor()
+        {
+            string line = this.reader.ReadLine();
+            if (line == null)
+            {
+                throw new EndOfStreamException();
+            }
+
+            string[] fields = line.Split(' ');
+            return Color.FromArgb(
+                int.Parse(fields[0]),
+                int.Parse(fields[1]),
+                int.Parse(fields[2]));
+        }
+
+        public void Dispose()
+        {
+            this.Dispose(true);
+        }
+
+        protected void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                this.reader.Dispose();
+            }
+        }
+    }
+}
