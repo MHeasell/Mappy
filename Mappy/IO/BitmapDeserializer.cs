297a2b6190dec8ec54d3893003e017bb8720b180
ArmouredFish <armouredfish@gmail.com>
2013-12-27 23:11:54 +0000

    Extract duplicate logic from bitmap deserializer classes

--- Mappy/IO/BitmapDeserializer.cs
+++ Mappy/IO/BitmapDeserializer.cs
@@ -1,10 +1,8 @@
 ﻿namespace Mappy.IO
 {
     using System.Drawing;
-    using System.Drawing.Imaging;
-    using System.IO;
 
-    public class BitmapDeserializer
+    public class BitmapDeserializer : AbstractBitmapDeserializer
     {
         private readonly Color[] palette;
 
@@ -13,31 +11,9 @@
             this.palette = palette;
         }
 
-        public Bitmap Deserialize(byte[] bytes, int width, int height)
+        protected override Color ToColor(int index)
         {
-            return this.Deserialize(new MemoryStream(bytes), width, height);
-        }
-
-        public Bitmap Deserialize(Stream bytes, int width, int height)
-        {
-            Bitmap bitmap = new Bitmap(width, height, PixelFormat.Format32bppArgb);
-            Rectangle rect = new Rectangle(new Point(0, 0), bitmap.Size);
-            BitmapData data = bitmap.LockBits(rect, ImageLockMode.WriteOnly, bitmap.PixelFormat);
-
-            int length = width * height;
-
-            unsafe
-            {
-                int* pointer = (int*)data.Scan0;
-                for (int i = 0; i < length; i++)
-                {
-                    pointer[i] = this.palette[bytes.ReadByte()].ToArgb();
-                }
-            }
-
-            bitmap.UnlockBits(data);
-
-            return bitmap;
+            return this.palette[index];
         }
     }
 }
--- Mappy/IO/KeyedBitmapDeserializer.cs
+++ Mappy/IO/KeyedBitmapDeserializer.cs
@@ -1,10 +1,8 @@
 ﻿namespace Mappy.IO
 {
     using System.Drawing;
-    using System.Drawing.Imaging;
-    using System.IO;
 
-    public class KeyedBitmapDeserializer
+    public class KeyedBitmapDeserializer : AbstractBitmapDeserializer
     {
         private readonly Color[] palette;
 
@@ -15,33 +13,9 @@
 
         public byte TransparencyKey { get; set; }
 
-        public Bitmap Deserialize(byte[] bytes, int width, int height)
+        protected override Color ToColor(int index)
         {
-            return this.Deserialize(new MemoryStream(bytes), width, height);
-        }
-
-        public Bitmap Deserialize(Stream bytes, int width, int height)
-        {
-            Bitmap bitmap = new Bitmap(width, height, PixelFormat.Format32bppArgb);
-            Rectangle rect = new Rectangle(new Point(0, 0), bitmap.Size);
-            BitmapData data = bitmap.LockBits(rect, ImageLockMode.WriteOnly, bitmap.PixelFormat);
-
-            int length = width * height;
-
-            unsafe
-            {
-                int* pointer = (int*)data.Scan0;
-                for (int i = 0; i < length; i++)
-                {
-                    int b = bytes.ReadByte();
-                    Color c = b == this.TransparencyKey ? Color.Transparent : this.palette[b];
-                    pointer[i] = c.ToArgb();
-                }
-            }
-
-            bitmap.UnlockBits(data);
-
-            return bitmap;
+            return index == this.TransparencyKey ? Color.Transparent : this.palette[index];
         }
     }
 }
