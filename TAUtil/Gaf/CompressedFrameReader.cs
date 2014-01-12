e30d2f477a0def545a5afe0368b4adf41628e4ad
ArmouredFish <armouredfish@gmail.com>
2014-01-12 12:58:13 +0000

    Handle too long and too short compressed GAF rows
    At least one GAF in the wild has been seen with too long compressed rows.
    e.g. TAK_Anims -> VeruTree -> VeruTree06,
    row 44-45ish decompresses to 109px, but the image is 108px in width.

--- TAUtil/Gaf/CompressedFrameReader.cs
+++ TAUtil/Gaf/CompressedFrameReader.cs
@@ -1,5 +1,6 @@
 ﻿namespace TAUtil.Gaf
 {
+    using System;
     using System.IO;
 
     public class CompressedFrameReader
@@ -20,78 +21,83 @@
 
             var s = new MemoryStream(data, true);
 
-            this.ReadCompressedImage(s, height);
+            this.ReadCompressedImage(s, width, height);
 
             return data;
         }
 
-        private void ReadCompressedImage(Stream output, int rows)
+        private static void RepeatByte(Stream output, byte value, int count)
         {
-            for (int i = 0; i < rows; i++)
-            {
-                this.ReadCompressedRow(output);
-            }
-        }
-
-        private void ReadCompressedRow(Stream output)
-        {
-            int bytes = this.reader.ReadUInt16();
-            long nextLinePos = this.reader.BaseStream.Position + bytes;
-
-            // while there are still bytes left in the line to read
-            while (this.reader.BaseStream.Position < nextLinePos)
-            {
-                this.ReadMaskedBlock(output);
-            }
-        }
-
-        private void ReadMaskedBlock(Stream output)
-        {
-            // read the mask
-            byte mask = this.reader.ReadByte();
-
-            if ((mask & 0x01) == 0x01)
-            {
-                // skip n pixels (transparency)
-                this.SkipBytes(output, mask >> 1);
-            }
-            else if ((mask & 0x02) == 0x02)
-            {
-                // repeat this byte n times
-                int count = (mask >> 2) + 1;
-                this.RepeatNextByte(output, count);
-            }
-            else
-            {
-                // copy next n bytes
-                int count = (mask >> 2) + 1;
-                this.CopyBytes(output, count);
-            }
-        }
-
-        private void SkipBytes(Stream output, int count)
-        {
-            for (int i = 0; i < count; i++)
-            {
-                output.WriteByte(this.transparencyIndex);
-            }
-        }
-
-        private void CopyBytes(Stream output, int count)
-        {
-            for (int i = 0; i < count; i++)
-            {
-                output.WriteByte(this.reader.ReadByte());
-            }
-        }
-
-        private void RepeatNextByte(Stream output, int count)
-        {
-            byte value = this.reader.ReadByte();
             for (int i = 0; i < count; i++)
             {
                 output.WriteByte(value);
             }
+        }
+
+        private void ReadCompressedImage(Stream output, int width, int height)
+        {
+            for (int i = 0; i < height; i++)
+            {
+                this.ReadCompressedRow(output, width);
+            }
+        }
+
+        private void ReadCompressedRow(Stream output, int rowLength)
+        {
+            int bytes = this.reader.ReadUInt16();
+
+            byte[] compressedRow = this.reader.ReadBytes(bytes);
+
+            this.DecompressRow(compressedRow, output, rowLength);
+        }
+
+        private void DecompressRow(byte[] compressedRow, Stream output, int rowLength)
+        {
+            var stream = new MemoryStream(compressedRow);
+            var reader = new BinaryReader(stream);
+
+            int bytesLeft = rowLength;
+
+            while (bytesLeft > 0 && stream.Position < compressedRow.Length)
+            {
+                bytesLeft -= this.DecompressBlock(reader, output, bytesLeft);
+            }
+
+            // Make up for any missing bytes if the row wasn't long enough
+            // after being decompressed.
+            RepeatByte(output, this.transparencyIndex, bytesLeft);
+        }
+
+        private int DecompressBlock(BinaryReader input, Stream output, int limit)
+        {
+            // read the mask
+            byte mask = input.ReadByte();
+
+            if ((mask & 0x01) == 0x01)
+            {
+                // skip n pixels (transparency)
+                int count = Math.Min(mask >> 1, limit);
+                RepeatByte(output, this.transparencyIndex, count);
+                return count;
+            }
+
+            if ((mask & 0x02) == 0x02)
+            {
+                // repeat this byte n times
+                int count = Math.Min((mask >> 2) + 1, limit);
+                byte val = input.ReadByte();
+                RepeatByte(output, val, count);
+                return count;
+            }
+
+            // by default, copy next n bytes
+            int bytesToRead = (mask >> 2) + 1;
+            byte[] buffer = input.ReadBytes(bytesToRead);
+
+            int bytesToWrite = Math.Min(bytesToRead, limit);
+            output.Write(buffer, 0, bytesToWrite);
+
+            return bytesToWrite;
         }
     }
 }
