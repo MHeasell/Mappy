MHeasell/Mappy

commit 76d23061c49e3540f57b2911bdeb0f4680b15854

Rewrite TNT and SCT IO logic
Reduces TAUtil's coupling to GDI+ Bitmap
and increases the general modularity of IO.

TAUtil/Tnt/TntWriter.cs
@@ -8,9 +8,13 @@
         private readonly BinaryWriter writer;
 
         public TntWriter(Stream s)
+            : this(new BinaryWriter(s))
         {
-            this.BaseStream = s;
-            this.writer = new BinaryWriter(s);
+        }
+
+        public TntWriter(BinaryWriter writer)
+        {
+            this.writer = writer;
         }
 
         ~TntWriter()
@@ -18,92 +22,38 @@
             this.Dispose(false);
         }
 
-        public Stream BaseStream { get; private set; }
-
-        public void WriteMinimap(int width, int height, byte[] data)
+        public void WriteTnt(ITntSource adapter)
         {
-            if (width <= 0 || height <= 0)
-            {
-                throw new ArgumentException("dimensions are invalid");
-            }
+            TntHeader h = new TntHeader();
+            h.IdVersion = TntHeader.TntMagicNumber;
 
-            if (width > TntConstants.MaxMinimapWidth || height > TntConstants.MaxMinimapHeight)
-            {
-                throw new ArgumentException("dimensions exceed maximum minimap bounds");
-            }
+            h.Width = (uint)(adapter.DataWidth * 2);
+            h.Height = (uint)(adapter.DataHeight * 2);
+            h.SeaLevel = (uint)adapter.SeaLevel;
 
-            if (data.Length != width * height)
-            {
-                throw new ArgumentException("data length does not match given dimensions");
-            }
+            h.TileAnims = (uint)adapter.AnimCount;
+            h.Tiles = (uint)adapter.TileCount;
 
-            this.writer.Write(TntConstants.MaxMinimapWidth);
-            this.writer.Write(TntConstants.MaxMinimapHeight);
+            int ptrAccumulator = TntHeader.HeaderLength;
+            h.PtrMapData = (uint)ptrAccumulator;
+            ptrAccumulator += sizeof(short) * adapter.DataWidth * adapter.DataHeight;
+            h.PtrMapAttr = (uint)ptrAccumulator;
+            ptrAccumulator += TileAttr.AttrLength * adapter.DataWidth * 2 * adapter.DataHeight * 2;
+            h.PtrTileGfx = (uint)ptrAccumulator;
+            ptrAccumulator += MapConstants.TileDataLength * adapter.TileCount;
+            h.PtrTileAnims = (uint)ptrAccumulator;
+            ptrAccumulator += (sizeof(int) + TntConstants.AnimNameLength) * adapter.AnimCount;
+            h.PtrMiniMap = (uint)ptrAccumulator;
 
-            for (int y = 0; y < TntConstants.MaxMinimapHeight; y++)
-            {
-                for (int x = 0; x < TntConstants.MaxMinimapWidth; x++)
-                {
-                    if (x >= width || y >= height)
-                    {
-                        this.writer.Write(TntConstants.MinimapVoidByte);
-                        continue;
-                    }
+            h.Write(this.writer);
 
-                    this.writer.Write(data[(y * width) + x]);
-                }
-            }
-        }
+            this.WriteData(adapter);
+            this.WriteAttrs(adapter);
+            this.WriteTiles(adapter);
+            this.WriteAnims(adapter);
+            this.WriteMinimap(adapter);
 
-        public void WriteTile(byte[] data)
-        {
-            if (data.Length != MapConstants.TileDataLength)
-            {
-                throw new ArgumentException("data is not tile sized");
-            }
-
-            this.writer.Write(data);
-        }
-
-        public void WritePixel(byte pixel)
-        {
-            this.writer.Write(pixel);
-        }
-
-        public void WriteAttr(TileAttr attr)
-        {
-            attr.Write(this.writer);
-        }
-
-        public void WriteAnim(string name)
-        {
-            // TA seems to ignore index field,
-            // so setting it to 0 is okay.
-            this.WriteAnim(name, 0);
-        }
-
-        public void WriteAnim(string name, int index)
-        {
-            if (name.Length >= TntConstants.AnimNameLength - 1)
-            {
-                throw new ArgumentException("name is too long");
-            }
-
-            if (index < 0)
-            {
-                throw new ArgumentException("index must not be negative");
-            }
-
-            byte[] c = new byte[TntConstants.AnimNameLength];
-            System.Text.Encoding.ASCII.GetBytes(name, 0, name.Length, c, 0);
-            this.writer.Write(index);
-            this.writer.Write(c);
-        }
-
-        public void WriteDataCell(short data)
-        {
-            BinaryWriter b = new BinaryWriter(this.BaseStream);
-            b.Write(data);
+            h.Unknown1 = 1; // if this is set to 0, the minimap doesn't show
         }
 
         public void Dispose()
@@ -115,8 +65,71 @@
         {
             if (disposing)
             {
-                this.BaseStream.Dispose();
+                this.writer.Dispose();
             }
+        }
+
+        private void WriteMinimap(ITntSource source)
+        {
+            MinimapInfo info = source.GetMinimap();
+
+            this.writer.Write(TntConstants.MaxMinimapWidth);
+            this.writer.Write(TntConstants.MaxMinimapHeight);
+
+            for (int y = 0; y < TntConstants.MaxMinimapHeight; y++)
+            {
+                for (int x = 0; x < TntConstants.MaxMinimapWidth; x++)
+                {
+                    if (y >= info.Height || x >= info.Width)
+                    {
+                        this.writer.Write(TntConstants.MinimapVoidByte);
+                        continue;
+                    }
+
+                    this.writer.Write(info.Data[(y * info.Width) + x]);
+                }
+            }
+        }
+
+        private void WriteAnims(ITntSource source)
+        {
+            int count = 0;
+            foreach (string anim in source.EnumerateAnims())
+            {
+                this.WriteAnim(anim, count++);
+            }
+        }
+
+        private void WriteTiles(ITntSource source)
+        {
+            foreach (byte[] tile in source.EnumerateTiles())
+            {
+                this.writer.Write(tile);
+            }
+        }
+
+        private void WriteAttrs(ITntSource source)
+        {
+            foreach (TileAttr t in source.EnumerateAttrs())
+            {
+                t.Write(this.writer);
+            }
+        }
+
+        private void WriteData(ITntSource source)
+        {
+            foreach (int i in source.EnumerateData())
+            {
+                this.writer.Write((short)i);
+            }
+        }
+
+        private void WriteAnim(string name, int index)
+        {
+            byte[] c = new byte[TntConstants.AnimNameLength];
+            System.Text.Encoding.ASCII.GetBytes(name, 0, name.Length, c, 0);
+            this.writer.Write(index);
+            this.writer.Write(c);
         }
     }
 }

Mappy/Data/Section.cs
@@ -49,9 +49,10 @@ namespace Mappy.Data
                 h.ExtractFile(this.cachedTilePath, outpath);
             }
 
-            using (SctReader s = new SctReader(File.OpenRead(outpath)))
+            using (var s = new SctReader(File.OpenRead(outpath)))
             {
-                this.cachedTile = TileIO.ReadFromSct(s);
+                SectionFactory factory = new SectionFactory(Globals.Palette);
+                this.cachedTile = factory.TileFromSct(s);
             }
 
             File.Delete(outpath);

Mappy/Globals.cs
@@ -9,7 +9,7 @@ namespace Mappy
     {
         public static readonly Color[] Palette;
 
-        public static readonly TileManager TileManager;
+        public static readonly BitmapCache TileCache;
 
         static Globals()
         {
@@ -18,7 +18,7 @@ namespace Mappy
                 Palette = Mappy.Util.Palette.LoadArr(s);
             }
 
-            TileManager = new TileManager(Palette);
+            TileCache = new BitmapCache();
         }
     }
 }

Mappy/IO/BitmapCache.cs
@@ -1,0 +1,59 @@
+﻿namespace Mappy.IO
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Drawing;
+    using System.Drawing.Imaging;
+    using System.Runtime.InteropServices;
+    using System.Security.Cryptography;
+
+    public class BitmapCache
+    {
+        private static readonly SHA1 Sha = SHA1.Create();
+
+        private readonly IDictionary<string, WeakReference> dictionary = new Dictionary<string, WeakReference>();
+
+        public Bitmap GetOrAddBitmap(Bitmap bitmap)
+        {
+            string hash = ComputeHash(bitmap);
+
+            WeakReference weakRef;
+            if (!this.dictionary.TryGetValue(hash, out weakRef))
+            {
+                // we didn't find the ref, so add one
+                this.dictionary[hash] = new WeakReference(bitmap);
+                return bitmap;
+            }
+
+            // we found a ref, see if we still have the bitmap
+            Bitmap b = weakRef.Target as Bitmap;
+            if (b == null)
+            {
+                // it's gone, replace it with the one we were given
+                weakRef.Target = bitmap;
+                return bitmap;
+            }
+
+            // If we got here,
+            // we managed to retrieve a live bitmap from cache
+            return b;
+        }
+
+        private static string ComputeHash(Bitmap bitmap)
+        {
+            BitmapData data = bitmap.LockBits(
+            new Rectangle(0, 0, bitmap.Width, bitmap.Height),
+            ImageLockMode.ReadOnly,
+            PixelFormat.Format32bppArgb);
+
+            int length = bitmap.Width * bitmap.Height * sizeof(int);
+            byte[] managedData = new byte[length];
+
+            Marshal.Copy(data.Scan0, managedData, 0, length);
+
+            bitmap.UnlockBits(data);
+
+            return Convert.ToBase64String(Sha.ComputeHash(managedData));
+        }
+    }
+}

Mappy/IO/BitmapDeserializer.cs
@@ -1,0 +1,43 @@
+﻿namespace Mappy.IO
+{
+    using System.Drawing;
+    using System.Drawing.Imaging;
+    using System.IO;
+
+    public class BitmapDeserializer
+    {
+        private readonly Color[] palette;
+
+        public BitmapDeserializer(Color[] palette)
+        {
+            this.palette = palette;
+        }
+
+        public Bitmap Deserialize(byte[] bytes, int width, int height)
+        {
+            return this.Deserialize(new MemoryStream(bytes), width, height);
+        }
+
+        public Bitmap Deserialize(Stream bytes, int width, int height)
+        {
+            Bitmap bitmap = new Bitmap(width, height, PixelFormat.Format32bppArgb);
+            Rectangle rect = new Rectangle(new Point(0, 0), bitmap.Size);
+            BitmapData data = bitmap.LockBits(rect, ImageLockMode.WriteOnly, bitmap.PixelFormat);
+
+            int length = width * height;
+
+            unsafe
+            {
+                int* pointer = (int*)data.Scan0;
+                for (int i = 0; i < length; i++)
+                {
+                    pointer[i] = this.palette[bytes.ReadByte()].ToArgb();
+                }
+            }
+
+            bitmap.UnlockBits(data);
+
+            return bitmap;
+        }
+    }
+}

Mappy/IO/BitmapSerializer.cs
@@ -1,0 +1,48 @@
+namespace Mappy.IO
+{
+    using System.Collections.Generic;
+    using System.Drawing;
+    using System.Drawing.Imaging;
+    using System.IO;
+
+    public class BitmapSerializer
+    {
+        private readonly IDictionary<Color, int> reversePalette;
+
+        public BitmapSerializer(IDictionary<Color, int> reversePalette)
+        {
+            this.reversePalette = reversePalette;
+        }
+
+        public byte[] ToBytes(Bitmap bitmap)
+        {
+            int length = bitmap.Width * bitmap.Height;
+
+            byte[] output = new byte[length];
+
+            this.Serialize(new MemoryStream(output, true), bitmap);
+
+            return output;
+        }
+
+        public void Serialize(Stream output, Bitmap bitmap)
+        {
+            Rectangle r = new Rectangle(0, 0, bitmap.Width, bitmap.Height);
+            BitmapData data = bitmap.LockBits(r, ImageLockMode.ReadOnly, bitmap.PixelFormat);
+
+            int length = bitmap.Width * bitmap.Height;
+
+            unsafe
+            {
+                int* pointer = (int*)data.Scan0;
+                for (int i = 0; i < length; i++)
+                {
+                    Color c = Color.FromArgb(pointer[i]);
+                    output.WriteByte((byte)this.reversePalette[c]);
+                }
+            }
+
+            bitmap.UnlockBits(data);
+        }
+    }
+}

Mappy/IO/MapIO.cs
@@ -1,260 +1,0 @@
-﻿namespace Mappy.IO
-{
-    using System;
-    using System.Collections.Generic;
-    using System.Diagnostics;
-    using System.Drawing;
-    using System.Drawing.Imaging;
-    using System.IO;
-    using System.Linq;
-
-    using Mappy.Data;
-    using Mappy.Models;
-    using Mappy.Util;
-
-    using TAUtil.Gaf;
-    using TAUtil.Tdf;
-    using TAUtil.Tnt;
-
-    public static class MapIO
-    {
-        private static readonly GafFrame DefaultFrame;
-
-        static MapIO()
-        {
-            MapIO.DefaultFrame.Offset = new Point(0, 0);
-            MapIO.DefaultFrame.Data = Mappy.Properties.Resources.nofeature;
-        }
-
-        public static MapModel Load(TntReader f, Color[] p, IDictionary<string, Feature> featureBank)
-        {
-            return LoadHelper(f, p, featureBank, new MapAttributes());
-        }
-
-        public static MapModel Load(TntReader f, TdfNode n, Color[] p, IDictionary<string, Feature> featureBank)
-        {
-            // load attrs
-            MapAttributes attrs = MapAttributes.Load(n);
-
-            return LoadHelper(f, p, featureBank, attrs);
-        }
-
-        public static void WriteTnt(MapModel map, TntWriter writer, Color[] palette)
-        {
-            var d = new Dictionary<Color, int>();
-            for (int i = 0; i < palette.Length; i++)
-            {
-                d[palette[i]] = i;
-            }
-
-            TntHeader h = new TntHeader();
-            h.IdVersion = TntHeader.TntMagicNumber;
-            
-            // FIXME: potentially unsafe cast?
-            h.Width = (uint)map.Tile.HeightGrid.Width;
-            h.Height = (uint)map.Tile.HeightGrid.Height;
-            h.SeaLevel = (uint)map.SeaLevel;
-
-            Bitmap[] tileMapping = Util.GetUsedTiles(map.Tile).ToArray();
-            h.Tiles = (uint)tileMapping.Length; // FIXME: conversion
-
-            Feature[] featureMapping = map.Features.Values.Distinct().ToArray();
-            h.TileAnims = (uint)featureMapping.Length; // FIXME: conversion
-
-            Stream s = writer.BaseStream;
-
-            // skip past the header to start writing
-            s.Seek(TntHeader.HeaderLength, SeekOrigin.Begin);
-
-            // write all the data
-            h.PtrMapData = (uint)s.Position;
-            WriteMapData(writer, map.Tile.TileGrid, tileMapping);
-
-            h.PtrMapAttr = (uint)s.Position;
-            foreach (TileAttr attr in EnumAsTileAttrs(map, featureMapping))
-            {
-                writer.WriteAttr(attr);
-            }
-
-            h.PtrTileGfx = (uint)s.Position;
-            WriteTiles(writer, tileMapping, d);
-
-            h.PtrTileAnims = (uint)s.Position;
-            foreach (string anim in featureMapping.Select(x => x.Name))
-            {
-                writer.WriteAnim(anim);
-            }
-
-            h.PtrMiniMap = (uint)s.Position;
-            writer.WriteMinimap(map.Minimap.Width, map.Minimap.Height, ConvertToBytes(map.Minimap, d));
-
-            h.Unknown1 = 1; // if this is set to 0, the minimap doesn't show
-
-            // skip back and write the header
-            s.Seek(0, SeekOrigin.Begin);
-            h.Write(s);
-        }
-
-        public static void WriteTiles(TntWriter writer, IEnumerable<Bitmap> tiles, IDictionary<Color, int> d)
-        {
-            foreach (Bitmap tile in tiles)
-            {
-                writer.WriteTile(ConvertToBytes(tile, d));
-            }
-        }
-
-        private static byte[] ConvertToBytes(Bitmap tile, IDictionary<Color, int> palette)
-        {
-            Rectangle r = new Rectangle(0, 0, tile.Width, tile.Height);
-            BitmapData data = tile.LockBits(r, ImageLockMode.ReadOnly, tile.PixelFormat);
-
-            int length = tile.Width * tile.Height;
-
-            byte[] output = new byte[length];
-
-            unsafe
-            {
-                int* pointer = (int*)data.Scan0;
-                for (int i = 0; i < length; i++)
-                {
-                    Color c = Color.FromArgb(pointer[i]);
-                    output[i] = (byte)palette[c];
-                }
-            }
-
-            tile.UnlockBits(data);
-
-            return output;
-        }
-
-        private static void WriteMapData(TntWriter writer, IEnumerable<Bitmap> data, Bitmap[] mapping)
-        {
-            var d = new Dictionary<Bitmap, short>();
-            for (short i = 0; i < mapping.Length; i++)
-            {
-                d[mapping[i]] = i;
-            }
-
-            foreach (Bitmap b in data)
-            {
-                writer.WriteDataCell(d[b]);
-            }
-        }
-
-        private static IEnumerable<TileAttr> EnumAsTileAttrs(MapModel map, Feature[] featureMapping)
-        {
-            for (int y = 0; y < map.Tile.HeightGrid.Height; y++)
-            {
-                for (int x = 0; x < map.Tile.HeightGrid.Width; x++)
-                {
-                    TileAttr attr = new TileAttr();
-                    attr.Height = (byte)map.Tile.HeightGrid.Get(x, y);
-                    Feature f;
-                    if (map.Features.TryGetValue(x, y, out f))
-                    {
-                        int i = Array.IndexOf(featureMapping, f);
-                        Debug.Assert(i != -1, "feature not found in mapping");
-                        attr.Feature = (ushort)i;
-                    }
-                    else if (map.Voids.HasValue(x, y))
-                    {
-                        attr.Feature = TileAttr.FeatureVoid;
-                    }
-                    else
-                    {
-                        attr.Feature = TileAttr.FeatureNone;
-                    }
-
-                    yield return attr;
-                }
-            }
-        }
-
-        private static Bitmap[] ReadTiles(TntReader reader)
-        {
-            Bitmap[] bitmaps = new Bitmap[reader.TileCount];
-
-            for (int i = 0; i < reader.TileCount; i++)
-            {
-                bitmaps[i] = Globals.TileManager.GetOrCreateBitmap(reader.ReadTile());
-            }
-
-            return bitmaps;
-        }
-
-        private static MapModel LoadHelper(TntReader f, Color[] p, IDictionary<string, Feature> featureBank, MapAttributes attrs)
-        {
-            // create the model
-            MapModel model = new MapModel(f.DataWidth, f.DataHeight, attrs);
-
-            // retrieve features
-            f.SeekToAnims();
-            Feature[] features = ReadFeatures(f, featureBank);
-
-            // retrieve tiles
-            f.SeekToTiles();
-            Bitmap[] tiles = ReadTiles(f);
-
-            // populate model tile data
-            f.SeekToData();
-            for (int y = 0; y < f.DataHeight; y++)
-            {
-                for (int x = 0; x < f.DataWidth; x++)
-                {
-                    model.Tile.TileGrid.Set(x, y, tiles[f.ReadDataCell()]);
-                }
-            }
-
-            // populate model heights, features, voids
-            f.SeekToAttrs();
-            for (int y = 0; y < f.Height; y++)
-            {
-                for (int x = 0; x < f.Width; x++)
-                {
-                    TileAttr t = f.ReadAttr();
-
-                    model.Tile.HeightGrid.Set(x, y, t.Height);
-
-                    switch (t.Feature)
-                    {
-                        case TileAttr.FeatureNone:
-                        case TileAttr.FeatureUnknown:
-                            break;
-                        case TileAttr.FeatureVoid:
-                            model.Voids.Set(x, y, true);
-                            break;
-                        default:
-                            model.Features.Set(x, y, features[t.Feature]);
-                            break;
-                    }
-                }
-            }
-
-            // don't forget the minimap
-            f.SeekToMinimap();
-            var minimap = f.ReadMinimap();
-            model.Minimap = Util.ReadToBitmap(minimap.Data, p, minimap.Width, minimap.Height);
-
-            // and sealevel
-            model.SeaLevel = f.SeaLevel;
-
-            return model;
-        }
-
-        private static Feature[] ReadFeatures(TntReader reader, IDictionary<string, Feature> bank)
-        {
-            Feature[] arr = new Feature[reader.AnimCount];
-
-            for (int i = 0; i < reader.AnimCount; i++)
-            {
-                string name = reader.ReadAnim();
-                if (!bank.TryGetValue(name, out arr[i]))
-                {
-                    arr[i] = new Feature(name, DefaultFrame.Data, DefaultFrame.Offset, new Size(1, 1));
-                }
-            }
-
-            return arr;
-        }
-    }
-}

Mappy/IO/MapModelFactory.cs
@@ -1,0 +1,130 @@
+﻿namespace Mappy.IO
+{
+    using System.Collections.Generic;
+    using System.Drawing;
+    using System.Linq;
+
+    using Mappy.Data;
+    using Mappy.Models;
+
+    using TAUtil;
+    using TAUtil.Gaf;
+    using TAUtil.Tnt;
+
+    public class MapModelFactory
+    {
+        private readonly BitmapDeserializer bitmapDeserializer;
+
+        private readonly IDictionary<string, Feature> featureBank;
+
+        private readonly GafFrame defaultFrame;
+
+        public MapModelFactory(
+            Color[] palette,
+            IDictionary<string, Feature> bank,
+            GafFrame defaultFrame)
+        {
+            this.bitmapDeserializer = new BitmapDeserializer(palette);
+            this.featureBank = bank;
+            this.defaultFrame = defaultFrame;
+        }
+
+        public MapModel FromTntAndOta(ITntSource tnt, MapAttributes ota)
+        {
+            MapModel m = new MapModel(tnt.DataWidth, tnt.DataHeight, ota);
+
+            return this.ReadTnt(tnt, m);
+        }
+
+        public MapModel FromTnt(ITntSource tnt)
+        {
+            MapModel m = new MapModel(tnt.DataWidth, tnt.DataHeight);
+
+            return this.ReadTnt(tnt, m);
+        }
+
+        private static void ReadFeatures(ITntSource tnt, MapModel model, List<Feature> features)
+        {
+            var enumer = tnt.EnumerateAttrs().GetEnumerator();
+            for (int y = 0; y < tnt.DataHeight * 2; y++)
+            {
+                for (int x = 0; x < tnt.DataWidth * 2; x++)
+                {
+                    enumer.MoveNext();
+                    model.Tile.HeightGrid.Set(x, y, enumer.Current.Height);
+
+                    switch (enumer.Current.Feature)
+                    {
+                        case TileAttr.FeatureNone:
+                        case TileAttr.FeatureUnknown:
+                            break;
+                        case TileAttr.FeatureVoid:
+                            model.Voids.Set(x, y, true);
+                            break;
+                        default:
+                            model.Features.Set(x, y, features[enumer.Current.Feature]);
+                            break;
+                    }
+                }
+            }
+        }
+
+        private static void ReadData(ITntSource tnt, MapModel model, List<Bitmap> tiles)
+        {
+            var enumer = tnt.EnumerateData().GetEnumerator();
+            for (int y = 0; y < tnt.DataHeight; y++)
+            {
+                for (int x = 0; x < tnt.DataWidth; x++)
+                {
+                    enumer.MoveNext();
+                    model.Tile.TileGrid.Set(x, y, tiles[enumer.Current]);
+                }
+            }
+        }
+
+        private MapModel ReadTnt(ITntSource tnt, MapModel model)
+        {
+            List<Bitmap> tiles = new List<Bitmap>(tnt.TileCount);
+            tiles.AddRange(tnt.EnumerateTiles().Select(this.ToBitmap));
+
+            ReadData(tnt, model, tiles);
+
+            List<Feature> features = new List<Feature>(tnt.AnimCount);
+            features.AddRange(tnt.EnumerateAnims().Select(this.ToFeature));
+
+            ReadFeatures(tnt, model, features);
+
+            model.SeaLevel = tnt.SeaLevel;
+
+            model.Minimap = this.ToBitmap(tnt.GetMinimap());
+
+            return model;
+        }
+
+        private Feature ToFeature(string name)
+        {
+            Feature f;
+            if (!this.featureBank.TryGetValue(name, out f))
+            {
+                f = new Feature(
+                    name,
+                    this.defaultFrame.Data,
+                    this.defaultFrame.Offset,
+                    new Size(1, 1));
+            }
+
+            return f;
+        }
+
+        private Bitmap ToBitmap(byte[] tile)
+        {
+            Bitmap bmp = this.bitmapDeserializer.Deserialize(tile, MapConstants.TileWidth, MapConstants.TileHeight);
+            return Globals.TileCache.GetOrAddBitmap(bmp);
+        }
+
+        private Bitmap ToBitmap(MinimapInfo minimap)
+        {
+            return this.bitmapDeserializer.Deserialize(minimap.Data, minimap.Width, minimap.Height);
+        }
+    }
+}

commit 126e3ef7c90ac62faea16e2a23fedb254c848318

Fix regression that prevents map previews from showing
Potentially introduced in 76d23061c49e3540f57b2911bdeb0f4680b15854

TAUtil/Tnt/TntWriter.cs
@@ -40,6 +40,8 @@
             ptrAccumulator += (sizeof(uint) + TntConstants.AnimNameLength) * adapter.AnimCount;
             h.PtrMiniMap = (uint)ptrAccumulator;
 
+            h.Unknown1 = 1; // if this is set to 0, the minimap doesn't show
+
             h.Write(this.writer);
 
             this.WriteData(adapter);
@@ -47,8 +49,6 @@
             this.WriteTiles(adapter);
             this.WriteAnims(adapter);
             this.WriteMinimap(adapter);
-
-            h.Unknown1 = 1; // if this is set to 0, the minimap doesn't show
         }
 
         public void Dispose()