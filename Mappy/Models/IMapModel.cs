d63c53962399425ffac6b4fc29888fd661a26c70
ArmouredFish <armouredfish@gmail.com>
2014-11-12 22:54:22 +0000

    Formatting

--- Mappy/Models/IMapModel.cs
+++ Mappy/Models/IMapModel.cs
@@ -49,19 +49,26 @@
         void UpdateFeatureInstance(FeatureInstance instance);
 
         /// <param name="id">The ID of the instance to retrieve.</param>
-        /// <returns>The feature instance with the given ID, or null if it is not found.</returns>
+        /// <returns>
+        /// The feature instance with the given ID,
+        /// or null if it is not found.
+        /// </returns>
         FeatureInstance GetFeatureInstance(Guid id);
 
         /// <param name="x">The x coordinate.</param>
         /// <param name="y">The y coordinate.</param>
-        /// <returns>True if the model contains a feature instance
-        /// at the specified location.</returns>
+        /// <returns>
+        /// True if the model contains a feature instance
+        /// at the specified location.
+        /// </returns>
         bool HasFeatureInstanceAt(int x, int y);
 
         /// <param name="x">The x coordinate.</param>
         /// <param name="y">The y coordinate.</param>
-        /// <returns>The feature instance at the given location,
-        /// or null if there is no feature instance at the location.</returns>
+        /// <returns>
+        /// The feature instance at the given location,
+        /// or null if there is no feature instance at the location.
+        /// </returns>
         FeatureInstance GetFeatureInstanceAt(int x, int y);
 
         /// <summary>
