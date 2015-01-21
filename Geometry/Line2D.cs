23680e706350623c645e821955fb1e6d0ee90e73
ArmouredFish <armouredfish@gmail.com>
2015-01-21 14:08:45 +0000

    Add 2D line and ray types to Geometry

---
+++ Geometry/Line2D.cs
@@ -0,0 +1,26 @@
+﻿namespace Geometry
+{
+    public struct Line2D
+    {
+        public Line2D(Vector2D start, Vector2D end)
+            : this()
+        {
+            this.Start = start;
+            this.End = end;
+        }
+
+        public Vector2D Start { get; set; }
+
+        public Vector2D End { get; set; }
+
+        public Ray2D ToRay()
+        {
+            return new Ray2D(this.Start, this.End - this.Start);
+        }
+
+        public Vector2D PointAt(double d)
+        {
+            return this.Start + ((this.End - this.Start) * d);
+        }
+    }
+}
---
+++ Geometry/Ray2D.cs
@@ -0,0 +1,20 @@
+﻿namespace Geometry
+{
+    public struct Ray2D
+    {
+        public Ray2D(Vector2D origin, Vector2D direction) : this()
+        {
+            this.Origin = origin;
+            this.Direction = direction;
+        }
+
+        public Vector2D Origin { get; set; }
+
+        public Vector2D Direction { get; set; }
+
+        public Vector2D PointAt(double t)
+        {
+            return this.Origin + (this.Direction * t);
+        }
+    }
+}
