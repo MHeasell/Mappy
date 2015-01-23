ffb74b5dc393b7bbdb83ac06a2f2ec387952c215
ArmouredFish <armouredfish@gmail.com>
2015-01-23 21:07:58 +0000

    Update TAUtil and fix project references
    TAUtil got split, so we now include the newly split project.

--- dist/MakeReleaseZip.py
+++ dist/MakeReleaseZip.py
@@ -37,6 +37,8 @@ dist_files = [
         join(slndir, "README.md"),
         join(bindir, "TAUtil.dll"),
         join(bindir, "TAUtil.pdb"),
+        join(bindir, "TAUtil.Hpi.dll"),
+        join(bindir, "TAUtil.Hpi.pdb"),
         join(bindir, "TAUtil.Gdi.dll"),
         join(bindir, "TAUtil.Gdi.pdb"),
     ]
