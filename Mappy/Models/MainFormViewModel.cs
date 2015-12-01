239fc5fcbdfc7dec83fec771e164c7bb18628555
ArmouredFish <armouredfish@gmail.com>
2015-12-01 00:12:31 +0000

    Replace duplicate code with method

--- Mappy/Models/MainFormViewModel.cs
+++ Mappy/Models/MainFormViewModel.cs
@@ -2,10 +2,7 @@
 {
     using System;
     using System.Collections.Generic;
-    using System.ComponentModel;
     using System.Drawing;
-    using System.Reactive.Linq;
-    using System.Reactive.Subjects;
 
     using Mappy.Data;
     using Mappy.Database;
@@ -18,130 +15,24 @@
         {
             this.model = model;
 
-            var modelPropertyChanged =
-                Observable.FromEventPattern<PropertyChangedEventHandler, PropertyChangedEventArgs>(
-                    x => this.model.PropertyChanged += x,
-                    x => this.model.PropertyChanged -= x)
-                    .Select(x => x.EventArgs.PropertyName);
+            this.CanUndo = model.PropertyAsObservable(x => x.CanUndo, "CanUndo");
+            this.CanRedo = model.PropertyAsObservable(x => x.CanRedo, "CanRedo");
+            this.CanCut = model.PropertyAsObservable(x => x.CanCut, "CanCut");
+            this.CanCopy = model.PropertyAsObservable(x => x.CanCopy, "CanCopy");
+            this.CanPaste = model.PropertyAsObservable(x => x.CanPaste, "CanPaste");
+            this.IsDirty = model.PropertyAsObservable(x => x.IsDirty, "IsDirty");
+            this.MapOpen = model.PropertyAsObservable(x => x.MapOpen, "MapOpen");
+            this.FilePath = model.PropertyAsObservable(x => x.FilePath, "FilePath");
+            this.IsFileReadOnly = model.PropertyAsObservable(x => x.IsFileReadOnly, "IsFileReadOnly");
+            this.GridVisible = model.PropertyAsObservable(x => x.GridVisible, "GridVisible");
+            this.GridSize = model.PropertyAsObservable(x => x.GridSize, "GridSize");
+            this.HeightmapVisible = model.PropertyAsObservable(x => x.HeightmapVisible, "HeightmapVisible");
+            this.FeaturesVisible = model.PropertyAsObservable(x => x.FeaturesVisible, "FeaturesVisible");
+            this.MinimapVisible = model.PropertyAsObservable(x => x.MinimapVisible, "MinimapVisible");
+            this.SeaLevel = model.PropertyAsObservable(x => x.SeaLevel, "SeaLevel");
 
-            var canUndo = new BehaviorSubject<bool>(this.model.CanUndo);
-            modelPropertyChanged
-                .Where(x => x == "CanUndo")
-                .Select(_ => this.model.CanUndo)
-                .Subscribe(canUndo);
-            this.CanUndo = canUndo;
-
-            var canRedo = new BehaviorSubject<bool>(this.model.CanRedo);
-            modelPropertyChanged
-                .Where(x => x == "CanRedo")
-                .Select(_ => this.model.CanRedo)
-                .Subscribe(canRedo);
-            this.CanRedo = canRedo;
-
-            var canCut = new BehaviorSubject<bool>(this.model.CanCut);
-            modelPropertyChanged
-                .Where(x => x == "CanCut")
-                .Select(_ => this.model.CanCut)
-                .Subscribe(canCut);
-            this.CanCut = canCut;
-
-            var canCopy = new BehaviorSubject<bool>(this.model.CanCopy);
-            modelPropertyChanged
-                .Where(x => x == "CanCopy")
-                .Select(_ => this.model.CanCopy)
-                .Subscribe(canCopy);
-            this.CanCopy = canCopy;
-
-            var canPaste = new BehaviorSubject<bool>(this.model.CanPaste);
-            modelPropertyChanged
-                .Where(x => x == "CanPaste")
-                .Select(_ => this.model.CanPaste)
-                .Subscribe(canPaste);
-            this.CanPaste = canPaste;
-
-            var isDirty = new BehaviorSubject<bool>(this.model.IsDirty);
-            modelPropertyChanged
-                .Where(x => x == "IsDirty")
-                .Select(_ => this.model.IsDirty)
-                .Subscribe(isDirty);
-            this.IsDirty = isDirty;
-
-            var mapOpen = new BehaviorSubject<bool>(this.model.MapOpen);
-            modelPropertyChanged
-                .Where(x => x == "MapOpen")
-                .Select(_ => this.model.MapOpen)
-                .Subscribe(mapOpen);
-            this.MapOpen = mapOpen;
-
-            var filePath = new BehaviorSubject<string>(this.model.FilePath);
-            modelPropertyChanged
-                .Where(x => x == "FilePath")
-                .Select(_ => this.model.FilePath)
-                .Subscribe(filePath);
-            this.FilePath = filePath;
-
-            var isFileReadOnly = new BehaviorSubject<bool>(this.model.IsFileReadOnly);
-            modelPropertyChanged
-                .Where(x => x == "IsFileReadOnly")
-                .Select(_ => this.model.IsFileReadOnly)
-                .Subscribe(isFileReadOnly);
-            this.IsFileReadOnly = isFileReadOnly;
-
-            var gridVisible = new BehaviorSubject<bool>(this.model.GridVisible);
-            modelPropertyChanged
-                .Where(x => x == "GridVisible")
-                .Select(_ => this.model.GridVisible)
-                .Subscribe(gridVisible);
-            this.GridVisible = gridVisible;
-
-            var gridSize = new BehaviorSubject<Size>(this.model.GridSize);
-            modelPropertyChanged
-                .Where(x => x == "GridSize")
-                .Select(_ => this.model.GridSize)
-                .Subscribe(gridSize);
-            this.GridSize = gridSize;
-
-            var heightmapVisible = new BehaviorSubject<bool>(this.model.HeightmapVisible);
-            modelPropertyChanged
-                .Where(x => x == "HeightmapVisible")
-                .Select(_ => this.model.HeightmapVisible)
-                .Subscribe(heightmapVisible);
-            this.HeightmapVisible = heightmapVisible;
-
-            var featuresVisible = new BehaviorSubject<bool>(this.model.FeaturesVisible);
-            modelPropertyChanged
-                .Where(x => x == "FeaturesVisible")
-                .Select(_ => this.model.FeaturesVisible)
-                .Subscribe(featuresVisible);
-            this.FeaturesVisible = featuresVisible;
-
-            var minimapVisible = new BehaviorSubject<bool>(this.model.MinimapVisible);
-            modelPropertyChanged
-                .Where(x => x == "MinimapVisible")
-                .Select(_ => this.model.MinimapVisible)
-                .Subscribe(minimapVisible);
-            this.MinimapVisible = minimapVisible;
-
-            var seaLevel = new BehaviorSubject<int>(this.model.SeaLevel);
-            modelPropertyChanged
-                .Where(x => x == "SeaLevel")
-                .Select(_ => this.model.SeaLevel)
-                .Subscribe(seaLevel);
-            this.SeaLevel = seaLevel;
-
-            var featureRecords = new BehaviorSubject<IFeatureDatabase>(this.model.FeatureRecords);
-            modelPropertyChanged
-                .Where(x => x == "FeatureRecords")
-                .Select(_ => this.model.FeatureRecords)
-                .Subscribe(featureRecords);
-            this.FeatureRecords = featureRecords;
-
-            var sections = new BehaviorSubject<IList<Section>>(this.model.Sections);
-            modelPropertyChanged
-                .Where(x => x == "Sections")
-                .Select(_ => this.model.Sections)
-                .Subscribe(sections);
-            this.Sections = sections;
+            this.FeatureRecords = model.PropertyAsObservable(x => x.FeatureRecords, "FeatureRecords");
+            this.Sections = model.PropertyAsObservable(x => x.Sections, "Sections");
         }
 
         public IObservable<IFeatureDatabase> FeatureRecords { get; }
