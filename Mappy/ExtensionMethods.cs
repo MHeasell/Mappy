e15a00dec4bc9d0f412bb4fa362162549744be1a
ArmouredFish <armouredfish@gmail.com>
2015-12-06 14:03:45 +0000

    Change PropertyAsObservable to return a BehaviorSubject
    This is basically a revert to the previous implementation,
    but with a concrete return type.

--- Mappy/ExtensionMethods.cs
+++ Mappy/ExtensionMethods.cs
@@ -7,21 +7,21 @@
 
     public static class ExtensionMethods
     {
-        public static IObservable<TField> PropertyAsObservable<TSource, TField>(this TSource source, Func<TSource, TField> accessor, string name) where TSource : INotifyPropertyChanged
+        public static BehaviorSubject<TField> PropertyAsObservable<TSource, TField>(this TSource source, Func<TSource, TField> accessor, string name) where TSource : INotifyPropertyChanged
         {
-            var obs = Observable.FromEventPattern<PropertyChangedEventHandler, PropertyChangedEventArgs>(
+            var subject = new BehaviorSubject<TField>(accessor(source));
+
+            // FIXME: This is leaky.
+            // We create a subscription here but don't hold on to the reference.
+            // We can never unregister our event handler without this.
+            Observable.FromEventPattern<PropertyChangedEventHandler, PropertyChangedEventArgs>(
                 x => source.PropertyChanged += x,
                 x => source.PropertyChanged -= x)
                 .Where(x => x.EventArgs.PropertyName == name)
                 .Select(_ => accessor(source))
-                .Multicast(new BehaviorSubject<TField>(accessor(source)));
+                .Subscribe(subject);
 
-            // FIXME: This is leaky.
-            // We create a connection here but don't hold on to the reference.
-            // We can never unregister our event handler without this.
-            obs.Connect();
-
-            return obs;
+            return subject;
         }
     }
 }
