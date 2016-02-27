MHeasell/Mappy

commit b062a694c59044974c28dd87a3423dab4075e4ae

Extract minimap import logic to dispatcher

Mappy/Models/UndoableMapModel.cs
@@ -20,9 +20,6 @@
     using Mappy.Util;
     using Mappy.Util.ImageSampling;
 
-    using TAUtil.Gdi.Palette;
-    using TAUtil.Tnt;
-
     public class UndoableMapModel : Notifier, IMainModel, IBandboxModel
     {
         private readonly OperationManager undoManager = new OperationManager();
@@ -148,7 +145,18 @@
 
         public bool IsMarked => this.undoManager.IsMarked;
 
-        public Bitmap Minimap => this.model.Minimap;
+        public Bitmap Minimap
+        {
+            get
+            {
+                return this.model.Minimap;
+            }
+
+            set
+            {
+                this.model.Minimap = value;
+            }
+        }
 
         public int SeaLevel => this.model.SeaLevel;
 
@@ -278,43 +286,6 @@
             this.undoManager.Execute(new CompositeOperation(
                 OperationFactory.CreateDeselectAndMergeOperation(this.model),
                 new SelectStartPositionOperation(this.model, index)));
-        }
-
-        public void ImportMinimap()
-        {
-            var loc = this.dialogService.AskUserToChooseMinimap();
-            if (loc == null)
-            {
-                return;
-            }
-
-            try
-            {
-                Bitmap bmp;
-                using (var s = File.OpenRead(loc))
-                {
-                    bmp = (Bitmap)Image.FromStream(s);
-                }
-
-                if (bmp.Width > TntConstants.MaxMinimapWidth
-                    || bmp.Height > TntConstants.MaxMinimapHeight)
-                {
-                    var msg = string.Format(
-                        "Minimap dimensions too large. The maximum size is {0}x{1}.",
-                        TntConstants.MaxMinimapWidth,
-                        TntConstants.MaxMinimapHeight);
-
-                    this.dialogService.ShowError(msg);
-                    return;
-                }
-
-                Quantization.ToTAPalette(bmp);
-                this.SetMinimap(bmp);
-            }
-            catch (Exception)
-            {
-                this.dialogService.ShowError("There was a problem importing the selected minimap.");
-            }
         }
 
         public void ImportCustomSection()

Mappy/Services/Dispatcher.cs
@@ -16,7 +16,9 @@
     using Mappy.Models;
 
     using TAUtil;
+    using TAUtil.Gdi.Palette;
     using TAUtil.Hpi;
+    using TAUtil.Tnt;
 
     public class Dispatcher
     {
@@ -341,7 +343,44 @@
 
         public void ImportMinimap()
         {
-            this.model.Map?.ImportMinimap();
+            if (this.model.Map == null)
+            {
+                return;
+            }
+
+            var loc = this.dialogService.AskUserToChooseMinimap();
+            if (loc == null)
+            {
+                return;
+            }
+
+            try
+            {
+                Bitmap bmp;
+                using (var s = File.OpenRead(loc))
+                {
+                    bmp = (Bitmap)Image.FromStream(s);
+                }
+
+                if (bmp.Width > TntConstants.MaxMinimapWidth
+                    || bmp.Height > TntConstants.MaxMinimapHeight)
+                {
+                    var msg = string.Format(
+                        "Minimap dimensions too large. The maximum size is {0}x{1}.",
+                        TntConstants.MaxMinimapWidth,
+                        TntConstants.MaxMinimapHeight);
+
+                    this.dialogService.ShowError(msg);
+                    return;
+                }
+
+                Quantization.ToTAPalette(bmp);
+                this.model.Map.Minimap = bmp;
+            }
+            catch (Exception)
+            {
+                this.dialogService.ShowError("There was a problem importing the selected minimap.");
+            }
         }
 
         public void ToggleFeatures()

commit eecfbec755d794f365adb3609b6a89a3cf400bcb

Fix minimap import not being undoable
Bug introduced by commit b062a694c59044974c28dd87a3423dab4075e4ae

Mappy/Models/UndoableMapModel.cs
@@ -145,18 +145,7 @@
 
         public bool IsMarked => this.undoManager.IsMarked;
 
-        public Bitmap Minimap
-        {
-            get
-            {
-                return this.model.Minimap;
-            }
-
-            set
-            {
-                this.model.Minimap = value;
-            }
-        }
+        public Bitmap Minimap => this.model.Minimap;
 
         public int SeaLevel => this.model.SeaLevel;
 
@@ -743,6 +732,12 @@
             this.undoManager.Execute(op);
         }
 
+        public void SetMinimap(Bitmap minimap)
+        {
+            var op = new UpdateMinimapOperation(this.model, minimap);
+            this.undoManager.Execute(op);
+        }
+
         private static void DeduplicateTiles(IGrid<Bitmap> tiles)
         {
             var len = tiles.Width * tiles.Height;
@@ -758,12 +753,6 @@
             int y = this.ViewportLocation.Y;
 
             this.AddAndSelectTile(tile, x, y);
-        }
-
-        private void SetMinimap(Bitmap minimap)
-        {
-            var op = new UpdateMinimapOperation(this.model, minimap);
-            this.undoManager.Execute(op);
         }
 
         private bool SaveHelper(string filename)

Mappy/Services/Dispatcher.cs
@@ -349,7 +349,7 @@
             }
 
             var minimap = this.LoadMinimapFromUser();
-            minimap.IfSome(x => this.model.Map.Minimap = x);
+            minimap.IfSome(x => this.model.Map.SetMinimap(x));
         }
 
         public void ToggleFeatures()