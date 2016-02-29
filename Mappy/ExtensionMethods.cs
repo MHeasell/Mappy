f3f94f28d7bb538bf810bc2d2905153a0319f5d6
ArmouredFish <armouredfish@gmail.com>
2016-02-29 22:23:14 +0000

    Rewrite PropertyAsObservable to fix resource leak

--- Mappy/ExtensionMethods.cs
+++ Mappy/ExtensionMethods.cs
@@ -3,26 +3,24 @@
     using System;
     using System.ComponentModel;
     using System.Reactive.Linq;
-    using System.Reactive.Subjects;
 
     public static class ExtensionMethods
     {
-        public static BehaviorSubject<TField> PropertyAsObservable<TSource, TField>(this TSource source, Func<TSource, TField> accessor, string name)
+        public static IObservable<TField> PropertyAsObservable<TSource, TField>(this TSource source, Func<TSource, TField> accessor, string name)
             where TSource : INotifyPropertyChanged
         {
-            var subject = new BehaviorSubject<TField>(accessor(source));
-
-            // FIXME: This is leaky.
-            // We create a subscription here but don't hold on to the reference.
-            // We can never unregister our event handler without this.
-            Observable.FromEventPattern<PropertyChangedEventHandler, PropertyChangedEventArgs>(
-                x => source.PropertyChanged += x,
-                x => source.PropertyChanged -= x)
-                .Where(x => x.EventArgs.PropertyName == name)
-                .Select(_ => accessor(source))
-                .Subscribe(subject);
-
-            return subject;
+            return Observable.Create<TField>(
+                observer =>
+                    {
+                        observer.OnNext(accessor(source));
+                        var values = Observable
+                            .FromEventPattern<PropertyChangedEventHandler, PropertyChangedEventArgs>(
+                                x => source.PropertyChanged += x,
+                                x => source.PropertyChanged -= x)
+                            .Where(x => x.EventArgs.PropertyName == name)
+                            .Select(_ => accessor(source));
+                        return values.Subscribe(observer);
+                    });
         }
 
         /// <summary>
