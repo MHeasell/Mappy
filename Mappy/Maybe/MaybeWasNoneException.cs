9c1e79ef3ee8d13b843bae050c5b6d97426d9b9a
ArmouredFish <armouredfish@gmail.com>
2016-03-01 23:05:04 +0000

    Add constructors to exception type

--- Mappy/Maybe/MaybeWasNoneException.cs
+++ Mappy/Maybe/MaybeWasNoneException.cs
@@ -1,9 +1,28 @@
 ﻿namespace Mappy.Maybe
 {
     using System;
+    using System.Runtime.Serialization;
 
     [Serializable]
     public class MaybeWasNoneException : Exception
     {
+        public MaybeWasNoneException()
+        {
+        }
+
+        public MaybeWasNoneException(string message)
+            : base(message)
+        {
+        }
+
+        public MaybeWasNoneException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+
+        protected MaybeWasNoneException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+        }
     }
 }
