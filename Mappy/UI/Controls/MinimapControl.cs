50038c4b62ffb8b04ddd74cc2913d0a485e6987c
ArmouredFish <armouredfish@gmail.com>
2016-03-05 12:24:02 +0000

    Add support for drawing markers in MinimapControl

--- Mappy/UI/Controls/MinimapControl.cs
+++ Mappy/UI/Controls/MinimapControl.cs
@@ -1,11 +1,15 @@
 ﻿namespace Mappy.UI.Controls
 {
+    using System.Collections.Generic;
     using System.ComponentModel;
     using System.Drawing;
+    using System.Linq;
     using System.Windows.Forms;
 
     public sealed class MinimapControl : Control
     {
+        private readonly Dictionary<int, MarkerInfo> markers = new Dictionary<int, MarkerInfo>();
+
         private bool rectVisible = true;
         private Color rectColor = Color.Black;
         private float rectThickness = 1.0f;
@@ -97,6 +101,29 @@
 
         private Rectangle CoveredRect => new Rectangle(this.ViewportRect.Location, this.ViewportRect.Size + new Size(1, 1));
 
+        public void SetMarker(int id, Point position, Color color)
+        {
+            MarkerInfo oldInfo;
+            if (this.markers.TryGetValue(id, out oldInfo))
+            {
+                this.InvalidateMarker(oldInfo.Position);
+            }
+
+            this.markers[id] = new MarkerInfo(position, color);
+            this.InvalidateMarker(position);
+        }
+
+        public void RemoveMarker(int id)
+        {
+            MarkerInfo oldInfo;
+            if (this.markers.TryGetValue(id, out oldInfo))
+            {
+                this.InvalidateMarker(oldInfo.Position);
+            }
+
+            this.markers.Remove(id);
+        }
+
         protected override void OnPaint(PaintEventArgs e)
         {
             base.OnPaint(e);
@@ -108,6 +135,47 @@
                     e.Graphics.DrawRectangle(p, this.ViewportRect);
                 }
             }
+
+            foreach (var marker in this.markers.Select(x => x.Value))
+            {
+                DrawMarker(e.Graphics, marker.Position, marker.Color);
+            }
+        }
+
+        private static void DrawMarker(Graphics g, Point position, Color color)
+        {
+            var bounds = GetMarkerBounds(position);
+            using (Brush b = new SolidBrush(color))
+            {
+                g.FillEllipse(b, bounds);
+            }
+        }
+
+        private static Rectangle GetMarkerBounds(Point position)
+        {
+            return new Rectangle(
+                position.X - 1,
+                position.Y - 1,
+                3,
+                3);
+        }
+
+        private void InvalidateMarker(Point position)
+        {
+            this.Invalidate(GetMarkerBounds(position));
+        }
+
+        private struct MarkerInfo
+        {
+            public MarkerInfo(Point position, Color color)
+            {
+                this.Position = position;
+                this.Color = color;
+            }
+
+            public Point Position { get; }
+
+            public Color Color { get; }
         }
     }
 }
