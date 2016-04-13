c7f28fb4bafdfbdee6aa4863ffd54676a94257d7
ArmouredFish <armouredfish@gmail.com>
2016-04-13 15:00:15 +0100

    Use absolute path to MSBuild in release script

--- dist/BuildAndRelease.bat
+++ dist/BuildAndRelease.bat
@@ -1,2 +1,2 @@
-MSBuild.exe ../Mappy.sln /p:Configuration=Release /t:Clean,Build && python MakeReleaseZip.py --release
+"C:/Program Files (x86)/MSBuild/14.0/bin/amd64/MSBuild.exe" ../Mappy.sln /p:Configuration=Release /t:Clean,Build && python MakeReleaseZip.py --release
 @pause
