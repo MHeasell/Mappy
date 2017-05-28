fe09acee4d3a0482ff3e4a83b477b2e12f47f0a9
ArmouredFish <armouredfish@gmail.com>
2017-05-28 16:00:17 +0100

    Tweak MSBuild location for VS2017 community

--- dist/BuildAndRelease.bat
+++ dist/BuildAndRelease.bat
@@ -1,2 +1,2 @@
-"C:/Program Files (x86)/MSBuild/14.0/bin/amd64/MSBuild.exe" ../Mappy.sln /p:Configuration=Release /t:Clean,Build && python MakeReleaseZip.py --release
+"C:/Program Files (x86)/Microsoft Visual Studio/2017/Community/MSBuild/15.0/Bin/MSBuild.exe" ../Mappy.sln /p:Configuration=Release /t:Clean,Build && python MakeReleaseZip.py --release
 @pause
