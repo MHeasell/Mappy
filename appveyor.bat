b886425be5d87f7408db08657ee995db5db38a77
ArmouredFish <armouredfish@gmail.com>
2018-04-01 10:32:13 +0100

    Set up appveyor

---
+++ appveyor.bat
@@ -0,0 +1,21 @@
+cd "%APPVEYOR_BUILD_FOLDER%" || goto :error
+
+rem Use python 3, not python 2
+SET PATH=C:\Python36-x64;%PATH%
+
+nuget restore Mappy.sln || goto :error
+msbuild Mappy.sln /p:Configuration=%Configuration% /t:Build /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll" || goto :error
+
+cd dist || goto :error
+IF "%Configuration%"=="Release" (
+    python MakeReleaseZip.py --release || goto :error
+) ELSE (
+    python MakeReleaseZip.py || goto :error
+)
+for %%i in (*.zip) do appveyor PushArtifact %%i
+
+goto :EOF
+
+
+:error
+exit /b %errorlevel%
