bfef8709e9719a924fec424624ed1a3216a5010c
Matthew Stubbins <dreamer@live.com.au>
2020-01-07 10:46:10 +1000

    Change to build script

--- dist/BuildAndRelease.bat
+++ dist/BuildAndRelease.bat
@@ -1,2 +1,2 @@
-"C:/Program Files (x86)/Microsoft Visual Studio/2017/Community/MSBuild/15.0/Bin/MSBuild.exe" ../Mappy.sln /p:Configuration=Release /t:Clean,Build && python MakeReleaseZip.py --release
+"A:/Programs/Microsoft Visual Studio/2019/Community/MSBuild/Current/Bin/MSBuild.exe" ../Mappy.sln /p:Configuration=Release /t:Clean,Build && python MakeReleaseZip.py --release
 @pause
